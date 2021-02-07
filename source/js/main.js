b03ca3dff3c2fec0a85105376809329206a3962c
ksd96 <45177616+ksd96@users.noreply.github.com>
2021-02-07 19:59:21 +0300

    добавляет логику сортировки

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@ import dataItems from './data';
 class Store {
   constructor(guitars) {
     this.guitars = guitars;
+
     this.filters = {
       type: this.getTypesGuitar(),
       strings: this.getStringsGuitar(),
@@ -11,9 +12,55 @@ class Store {
         max: this.getMaxGuitarsPrice()
       }
     };
+
+    this.sort = {
+      price: null,
+      popularity: null
+    };
   }
 
-  getFilters() {
+  // получает значение сортировки по цене
+  setSortPrice(value) {
+    this.sort.price = value;
+  }
+
+  // получает значение сортировки по популярности
+  setSortPopularity(value) {
+    this.sort.popularity = value;
+  }
+
+  // сортирует по цене
+  getSortPrice() {
+    const guitars = this.getFilteredGuitars();
+    if (this.sort.price === `min`) {
+       guitars.sort((a, b) => {
+         return a.price - b.price;
+       });
+    } else if (this.sort.price === `max`) {
+      guitars.sort((a, b) => {
+        return b.price - a.price;
+      });
+    }
+    return guitars;
+  }
+
+  // сортирует по популярности
+  getSortPopularity() {
+    const guitars = this.getFilteredGuitars();
+    if (this.sort.popularity === `min`) {
+       guitars.sort((a, b) => {
+         return a.popularity - b.popularity;
+       });
+    } else if (this.sort.popularity === `max`) {
+      guitars.sort((a, b) => {
+        return b.popularity - a.popularity;
+      });
+    }
+    return guitars;
+  }
+
+  // отдает текущие значения фильтров
+  getFiltersValue() {
     const filters = {
       type: this.filters.type,
       strings: this.filters.strings,
@@ -24,27 +71,32 @@ class Store {
     return filters;
   }
 
+  // отдает существующие типы гитар
   getTypesGuitar() {
     const types = Object.values(this.guitars).map((guitar) => guitar.type);
     return new Set(types);
   }
 
+  // отдает существующие типы гитар по колличеству струн
   getStringsGuitar() {
     const strings = Object.values(this.guitars).map((guitar) => guitar.strings);
     return new Set(strings);
   }
 
+  // отдает максимальную стоимость гитар
   getMaxGuitarsPrice() {
     const prices = Object.values(this.guitars).map((guitar) => guitar.price);
     return Math.max.apply(null, prices);
   }
 
+  // отдает минимальную стоимость гитар
   getMinGuitarsPrice() {
 		const prices = Object.values(this.guitars).map((guitar) => guitar.price);
     return Math.min.apply(null, prices);
   }
 
-  sortByType(cards, types) {
+  // фильтрует по типу
+  filteringByType(cards, types) {
     if (types.length === 0) {
       return cards;
     } else {
@@ -60,7 +112,8 @@ class Store {
     }
   }
 
-  sortByStrings(cards, strings) {
+  // фильтрует по колличеству струн
+  filteringByStrings(cards, strings) {
     if (strings.length === 0) {
       return cards;
     } else {
@@ -76,11 +129,13 @@ class Store {
     }
   }
 
-  sortByPrice(cards, minPrice, maxPrice) {
+  // фильтрует по цене
+  filteringByPrice(cards, minPrice, maxPrice) {
     const guitars = Object.values(cards).filter((guitar) => guitar.price >= minPrice && guitar.price <= maxPrice);
     return guitars;
   }
 
+  // получает новые значения фильтров
   setFilters(objectData) {
     this.filters.type = objectData.type;
     this.filters.strings = objectData.strings;
@@ -88,68 +143,88 @@ class Store {
     this.filters.price.max = objectData.priceMax;
   }
 
+  // отдает отфильтрованный массив гитар
   getFilteredGuitars() {
     let filteredCards = this.guitars;
-    filteredCards = this.sortByType(filteredCards, this.filters.type);
-    filteredCards = this.sortByStrings(filteredCards, this.filters.strings);
-    filteredCards = this.sortByPrice(filteredCards, this.filters.price.min, this.filters.price.max);
+    filteredCards = this.filteringByType(filteredCards, this.filters.type);
+    filteredCards = this.filteringByStrings(filteredCards, this.filters.strings);
+    filteredCards = this.filteringByPrice(filteredCards, this.filters.price.min, this.filters.price.max);
     return filteredCards;
-  }
-
-  getGuitars() {
-    return Object.values(this.guitars);
   }
 }
 
-// class View {
-//   _getGuitarCard(guitar) {
-//     const template = document.querySelector('#cardTemplate');
-//     const clone = template.content.cloneNode(true);
-//     clone.querySelector('.card__title').textContent = guitar.name;
-//     clone.querySelector('.card__price').textContent = guitar.price;
-//     clone.querySelector('.card__rating').textContent = guitar.popularity;
-//     return clone;
-//   }
+class View {
+  _getGuitarCard(guitar) {
+    const template = document.querySelector('#cardTemplate');
+    const clone = template.content.cloneNode(true);
+    clone.querySelector('.card__title').textContent = guitar.name;
+    clone.querySelector('.card__price').textContent = guitar.price;
+    clone.querySelector('.card__rating').textContent = guitar.popularity;
+    return clone;
+  }
 
-//   renderCards(guitars) {
+  renderCards(guitars) {
+    const container = document.querySelector('.cards__list');
 
-//     const container = document.querySelector('.main');
-//     // очищаем
-//     // while (container.firstChild) {
-//     //   container.removeChild(container.lastChild);
-//     // }
+    // очищаем
+    while (container.firstChild) {
+      container.removeChild(container.lastChild);
+    }
 
-//     // рисуем новые карточки
-//     guitars.forEach((guitar) => {
-//       const card = this._getGuitarCard(guitar);
-//       container.appendChild(card);
-//     });
-//   }
-// }
+    // рисуем новые карточки
+    guitars.forEach((guitar) => {
+      const card = this._getGuitarCard(guitar);
+      container.appendChild(card);
+    });
+  }
+}
 
-// class Presenter {
-//   constructor(store, view) {
-//     this.store = store;
-//     this.view = view;
-//   }
+class Presenter {
+  constructor(store, view) {
+    this.store = store;
+    this.view = view;
+    this.buttonSortPrice = document.querySelector(`.sort__button-type_type_price`);
+    this.buttonSortPopularity = document.querySelector(`.sort__button-type_type_popularity`);
+    this.buttonSortMin = document.querySelector(`.sort__button_type_min`);
+    this.buttonSortMax = document.querySelector(`.sort__button_type_max`);
+  }
 
-//   init() {
-//     this.renderGuitars();
-//   }
+  init() {
+    this.renderGuitars();
+    this.buttonsListner();
+  }
 
-//   renderGuitars() {
-//     this.view.renderCards(this.store.getGuitars());
-//   }
+  buttonsListner() {
+    this.buttonSortPrice.addEventListener(`click`, () => {
+      this.changeButtonsStates(this.buttonSortPrice, `sort__button-type`);
+    });
+    this.buttonSortPopularity.addEventListener(`click`, () => {
+      this.changeButtonsStates(this.buttonSortPopularity, `sort__button-type`);
+    });
+    this.buttonSortMin.addEventListener(`click`, () => {
+      this.changeButtonsStates(this.buttonSortMin, `sort__button`);
+    });
+    this.buttonSortMax.addEventListener(`click`, () => {
+      this.changeButtonsStates(this.buttonSortMax, `sort__button`);
+    });
+  }
 
-// }
+  changeButtonsStates(item, name) {
+    const buttons = document.querySelectorAll(`.${name}_active`);
+    buttons.forEach((button) => {
+      button.classList.remove(`${name}_active`);
+    });
+    item.classList.add(`${name}_active`);
+  }
+
+  renderGuitars() {
+    this.view.renderCards(this.store.getFilteredGuitars());
+  }
+
+}
 
 const myStore = new Store(dataItems);
-// const myView = new View();
-// const myPresenter = new Presenter(myStore, myView);
-console.log(myStore.getFilters());
-console.log(myStore.getGuitars());
-myStore.setFilters({type: [], strings: [], priceMin: 5000, priceMax: 20000});
-console.log(myStore.getFilteredGuitars());
-console.log(myStore.getFilters());
+const myView = new View();
+const myPresenter = new Presenter(myStore, myView);
 
-// myPresenter.init();
+myPresenter.init();
